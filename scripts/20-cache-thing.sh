a0b5240a41bef87230ed2a559353b333e1a8976f
Nils Ponsard <nils.ponsard@inria.fr>
2025-11-19 11:21:13 +0100

    fix: enable git cache later
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- scripts/20-cache-thing.sh
+++ scripts/20-cache-thing.sh
@@ -1,4 +1,3 @@
-# 
-
+# Install cache-thing, needs to run before the activation of git-cache
 
 cargo +${RUST_TOOLS_TOOLCHAIN} install cache-thing --git https://github.com/nponsard/cache-thing.git --branch feat/btrfs                                     
