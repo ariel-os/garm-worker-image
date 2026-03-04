d8124b13af0c867dab1855498ddd0b3bcd10abab
Nils Ponsard <nils.ponsard@inria.fr>
2026-03-04 16:24:30 +0100

    fix: always install with --locked
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- scripts/20-cache-thing.sh
+++ scripts/20-cache-thing.sh
@@ -1,3 +1,3 @@
 # Install cache-thing, needs to run before the activation of git-cache
 
-cargo +${RUST_TOOLS_TOOLCHAIN} install cache-thing --git https://github.com/nponsard/cache-thing.git --tag 0.3.2
+cargo +${RUST_TOOLS_TOOLCHAIN} install cache-thing --git https://github.com/nponsard/cache-thing.git --tag 0.3.2 --locked
